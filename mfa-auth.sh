206cbc34b6f900f1e7a5296fd08a40202e198643
Jihan Lee <alan@sync.day>
2023-04-02 20:10:11 +0900

    Add mfa auth script

---
+++ mfa-auth.sh
@@ -0,0 +1,51 @@
+
+echo -n "Set MFA profile name (default mfa): "
+read MFA_PROFILE_NAME
+
+if [ -z "$MFA_PROFILE_NAME" ]; then
+    MFA_PROFILE_NAME=mfa
+fi
+
+# 36 hours
+SESSION_TIME=129600
+# Seoul
+DEFAULT_REGION=ap-northeast-2
+
+function install_check() {
+    which $1 > /dev/null;
+
+    if [[ $? -ne 0 ]]; then
+        echo "Please install $1."
+        exit 1;
+    else
+        echo "$1 install is checked."
+    fi
+}
+
+# check aws cli
+install_check "aws"
+
+# check jq cli
+install_check "jq"
+
+AWS_USER_ID=$(aws sts get-caller-identity | jq -r ".UserId")
+MFA_DEVICE_ARN=$(aws iam list-virtual-mfa-devices | jq -r '.VirtualMFADevices[] | select(.User.UserId == "'$AWS_USER_ID'") | .SerialNumber')
+
+echo "Detected ARN: "$MFA_DEVICE_ARN
+
+echo -n "Input OTP:"
+read TOKEN_CODE
+
+echo aws sts get-session-token --serial-number $MFA_DEVICE_ARN --token-code $TOKEN_CODE --duration-seconds $SESSION_TIME
+MFA_PROFILE=$(aws sts get-session-token --serial-number $MFA_DEVICE_ARN --token-code $TOKEN_CODE --duration-seconds $SESSION_TIME)
+
+echo $MFA_PROFILE | jq
+MFA_PROFILE_ACCESS_KEY=$(echo $MFA_PROFILE | jq -r ".Credentials.AccessKeyId")
+MFA_PROFILE_SECRET_ACCESS_KEY=$(echo $MFA_PROFILE | jq -r ".Credentials.SecretAccessKey")
+MFA_PROFILE_SESSION_TOKEN=$(echo $MFA_PROFILE | jq -r ".Credentials.SessionToken")
+
+aws configure set --profile $MFA_PROFILE_NAME region $DEFAULT_REGION
+aws configure set --profile $MFA_PROFILE_NAME aws_access_key_id $MFA_PROFILE_ACCESS_KEY
+aws configure set --profile $MFA_PROFILE_NAME aws_secret_access_key $MFA_PROFILE_SECRET_ACCESS_KEY
+aws configure set --profile $MFA_PROFILE_NAME aws_session_token $MFA_PROFILE_SESSION_TOKEN
+aws configure set --profile $MFA_PROFILE_NAME output json
